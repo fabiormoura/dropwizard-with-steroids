5c52998380ad4ba2e5c371f912c3b75be60317c3
Fabio Maia <fabioromm@gmail.com>
2015-08-16 22:20:19 -0300

    Create sample test case to validate if gradle test task works

---
+++ quickstart/src/test/java/com/dropwizard/steroids/quickstart/hello/HelloWorldResourceTest.java
@@ -0,0 +1,12 @@
+package com.dropwizard.steroids.quickstart.hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldResourceTest {
+    @Test
+    public void shouldBeTrue() {
+        assertTrue(true);
+    }
+}
